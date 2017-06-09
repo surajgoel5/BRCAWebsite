12fbc627d6b4731d7361806ed8cb7cd4cc805caa
Utkarsh Singh <utkarshsinghoct1997@gmail.com>
2017-06-09 19:59:49 +0530

    Updated brcarecognition.php

--- brcarecognition.php
+++ brcarecognition.php
@@ -3,71 +3,64 @@
 <html>
 	<head>
 		<title>BRCA, IIT Delhi</title>
-
-
-
-
-    <!--ninjaVideoPlugin.js is required only when the slider contains videos, and its link should be placed before the ninja-slider.js link.-->
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
-
-
-<link rel="stylesheet" href="assets/css/animations.css">
-
-<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
 		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/sacbooking.css" />
-		<link rel="stylesheet" href="magnific-popup/magnific-popup.css">
-		
-
-		
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>
+
+
+        <!-- Bootstrap Core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/animations.css">
+
+
+        <!-- Custom CSS -->
+     	<link rel="stylesheet" href="magnific-popup/magnific-popup.css">
+    
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+        <link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css">
+        <link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
+		
 		
 	</head>
 	<body class="homepage">
 		<div id="page-wrapper">
 
-			<!-- Header -->
-<div id="header" class="animatedParent animateOnce" data-sequence='700' >
+		<!-- Header -->
+            <div id="header">
 
-					<!-- Inner -->
-						
+				<!-- Inner -->
+					<div class="animatedParent animateOnce" data-sequence='700'>	
+                        <header>
+                            <h1><a id="logo" class="animated fadeIn slower" data-id='1'>BRCA Recognitions</a></h1>
+                        </header>
+                        <footer class="foot">
+                            <section class="color-4">
+				                <nav class="cl-effect-14">
+					                <a class="animated fadeIn slower" data-id='2' href="13-14.php">2013-14</a>
+					                <a class="animated fadeIn slower" data-id='2' href="14-15.php">2014-15</a>
+					                <a class="animated fadeIn slower" data-id='2' href="15-16.php">2015-16</a>
+					                <a class="animated fadeIn slower" data-id='2' href="16-17.php">2016-17</a>
+				                </nav>
+			                </section>
+			            </footer>
+			        </div>
 
+		        <!-- Nav -->				
+                    <?php include 'menu.php';?>
+            </div>
 
-<section class="color-4">
-				<nav class="cl-effect-14">
-					
-					
-					<a href="13-14.php">2013-14</a>
-					<a href="14-15.php">2014-15</a>
-					<a href="15-16.php">2015-16</a>
-					<a href="16-17.php">2016-17</a>
-
-				</nav>
-			</section>
-
-
-					<!-- Nav -->
-						
-<?php include 'menu.php';?>
-</div>
-
-</div>
-						
-			
+        </div>
 				
-
-<?php include ('footer.php');?>
-</div>
-    
-
-        
-
-
-			
+        <?php include ('footer.php');?>
+    </div>
 
 		<!-- Scripts -->
 			<script src="assets/js/jquery.min.js"></script>
@@ -81,23 +74,7 @@
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			
 			<script src="assets/js/main.js"></script>
-			<script src="magnific-popup/jquery.magnific-popup.js"></script>
-
-
-			
-
-
-
-
-
-			
-
-
-
-		
-			
-
-				
+			<script src="magnific-popup/jquery.magnific-popup.js"></script>		
 
 	</body>
 </html>
